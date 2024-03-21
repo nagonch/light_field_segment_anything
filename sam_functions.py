b838a5483e2748f705c55094d5d24ee8c4fcf82d
nagonch <kolchindru@gmail.com>
2024-03-21 15:10:04 +1100

    batched embedding extraction

--- sam_functions.py
+++ sam_functions.py
@@ -22,6 +22,7 @@ class SimpleSAM(nn.Module):
         self.sam = sam
         self.sparse_prompt_emb, self.dense_prompt_emb = self.get_prompt_embeddings()
 
+    @torch.no_grad()
     def get_prompt_embeddings(self, n_points_per_side=32):
         input_points = torch.tensor(
             build_all_layer_point_grids(n_points_per_side, 0, 1)[0]
@@ -36,6 +37,7 @@ class SimpleSAM(nn.Module):
 
         return sparse_prompt_embedding, dense_prompt_embedding
 
+    @torch.no_grad()
     def forward(self, imgs):
         imgs = torch.stack([self.sam.preprocess(x) for x in imgs], dim=0)
         image_embeddings = self.sam.image_encoder(imgs)
@@ -51,7 +53,7 @@ if __name__ == "__main__":
     from torchvision.transforms.functional import resize
 
     sam = get_sam(return_generator=False)
-    predictor = SamPredictor(sam)
+    simple_sam = SimpleSAM(sam)
     dir = "/home/cedaradmin/data/lf_angular/LFPlane/f00051/png"
     subviews = []
     for img in list(sorted(os.listdir(dir))):
@@ -59,19 +61,11 @@ if __name__ == "__main__":
         subviews.append(np.array(Image.open(path))[:, :, :3])
     LF = np.stack(subviews).reshape(17, 17, 128, 128, 3).astype(np.uint8)
     img = LF[0][0]
-    batch = (
-        torch.as_tensor(LF[0:2, 0], device=predictor.device)
-        .permute(0, -1, 1, 2)
-        .float()[:1]
-    )
+    batch = (torch.as_tensor(LF[0:2, 0]).permute(0, -1, 1, 2).float()).cuda()
     batch = resize(
         batch.reshape(-1, batch.shape[-2], batch.shape[-1]),
         (1024, 1024),
         antialias=True,
     ).reshape(batch.shape[0], 3, 1024, 1024)
-    batch = batch.contiguous()
-    batch = predictor.model.preprocess(batch)
-    predictor.original_size = (128, 128)
-    with torch.no_grad():
-        feature = predictor.model.image_encoder(batch)
-    print(feature.shape)
+    embeddings = simple_sam(batch)
+    print(embeddings.shape)
