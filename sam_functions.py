721bd4eda685f3dae965329c7110952561454ede
nagonch <kolchindru@gmail.com>
2024-03-21 19:07:05 +1100

    batch output

--- sam_functions.py
+++ sam_functions.py
@@ -4,6 +4,7 @@ from segment_anything.utils.amg import build_all_layer_point_grids
 import torch
 from torch import nn
 from torchvision.transforms.functional import resize
+from segment_anything.utils.amg import batch_iterator
 
 
 def get_sam(return_generator=True):
@@ -17,10 +18,11 @@ def get_sam(return_generator=True):
 
 
 class SimpleSAM(nn.Module):
-    def __init__(self, sam, img_size=(128, 128)):
+    def __init__(self, sam, img_size=(128, 128), points_per_batch=64):
         super().__init__()
         self.img_size = img_size
         self.sam = sam
+        self.points_per_batch = points_per_batch
         self.sparse_prompt_emb, self.dense_prompt_emb = self.get_prompt_embeddings()
 
     @torch.no_grad()
@@ -57,30 +59,57 @@ class SimpleSAM(nn.Module):
     def forward(
         self,
         batch,
-        return_logits=False,
+        return_logits=True,
     ):
         batch = self.preprocess_batch(batch)
         image_embeddings = self.sam.image_encoder(batch)
-        sparse_emb = self.sparse_prompt_emb[:64]
-        dense_emb = self.dense_prompt_emb[:64]
-        low_res_masks, iou_predictions = self.sam.mask_decoder(
-            image_embeddings=image_embeddings,
-            image_pe=self.sam.prompt_encoder.get_dense_pe(),
-            sparse_prompt_embeddings=sparse_emb,
-            dense_prompt_embeddings=dense_emb,
-            multimask_output=True,
+        batch_iteration = zip(
+            batch_iterator(self.points_per_batch, self.sparse_prompt_emb),
+            batch_iterator(self.points_per_batch, self.dense_prompt_emb),
         )
-        masks = self.sam.postprocess_masks(
-            low_res_masks,
-            input_size=batch.shape[-2:],
-            original_size=self.img_size,
+        masks_batches = []
+        iou_predictions_batches = []
+        low_res_logits_batches = []
+        for sparse_emb, dense_emb in batch_iteration:
+            low_res_masks, iou_predictions = self.sam.mask_decoder(
+                image_embeddings=image_embeddings,
+                image_pe=self.sam.prompt_encoder.get_dense_pe(),
+                sparse_prompt_embeddings=sparse_emb[0],
+                dense_prompt_embeddings=dense_emb[0],
+                multimask_output=True,
+            )
+            batch_shape, n_points, c, w, h = low_res_masks.shape
+            masks = self.sam.postprocess_masks(
+                low_res_masks.reshape(-1, c, w, h),
+                input_size=batch.shape[-2:],
+                original_size=self.img_size,
+            )
+            masks = masks.reshape(
+                batch_shape, n_points, c, masks.shape[-2], masks.shape[-1]
+            )
+            if not return_logits:
+                masks = masks > self.sam.mask_threshold
+            masks_batches.append(masks)
+            iou_predictions_batches.append(iou_predictions)
+            low_res_logits_batches.append(low_res_masks)
+        masks = torch.stack(masks_batches).permute(1, 0, 2, 3, 4, 5)
+        iou_predictions = torch.stack(iou_predictions_batches).permute(1, 0, 2, 3)
+        low_res_logits = torch.stack(low_res_logits_batches).permute(1, 0, 2, 3, 4, 5)
+        masks = masks.reshape(masks.shape[0], -1, masks.shape[-2], masks.shape[-1])
+        iou_predictions = iou_predictions.reshape(
+            iou_predictions.shape[0],
+            -1,
         )
-        if not return_logits:
-            masks = masks > self.sam.mask_threshold
+        low_res_logits = low_res_logits.reshape(
+            low_res_logits.shape[0],
+            -1,
+            low_res_logits.shape[-2],
+            low_res_logits.shape[-1],
+        )
         return {
             "masks": masks,
             "iou_predictions": iou_predictions,
-            "low_res_logits": low_res_masks,
+            "low_res_logits": low_res_logits,
         }
 
 
@@ -98,9 +127,9 @@ if __name__ == "__main__":
     simple_sam = SimpleSAM(sam)
     dir = "/home/cedaradmin/blender/lightfield/LFPlane/f00051/png"
     LF = get_LF(dir)
-    batch = (torch.as_tensor(LF[0:2, 0]).permute(0, -1, 1, 2).float()).cuda()
+    batch = (torch.as_tensor(LF[0:2, 0]).permute(0, -1, 1, 2).float()).cuda()[:1]
     result = simple_sam(batch)
-    masks = result["masks"].to(torch.int32).flatten(0, 1).detach().cpu().numpy()
+    masks = result["masks"].to(torch.int32)[0].detach().cpu().numpy()
     for mask in masks:
         plt.imshow(mask, cmap="gray")
         plt.show()
--- segment_anything/modeling/mask_decoder.py
+++ segment_anything/modeling/mask_decoder.py
@@ -51,10 +51,14 @@ class MaskDecoder(nn.Module):
         self.mask_tokens = nn.Embedding(self.num_mask_tokens, transformer_dim)
 
         self.output_upscaling = nn.Sequential(
-            nn.ConvTranspose2d(transformer_dim, transformer_dim // 4, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(
+                transformer_dim, transformer_dim // 4, kernel_size=2, stride=2
+            ),
             LayerNorm2d(transformer_dim // 4),
             activation(),
-            nn.ConvTranspose2d(transformer_dim // 4, transformer_dim // 8, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(
+                transformer_dim // 4, transformer_dim // 8, kernel_size=2, stride=2
+            ),
             activation(),
         )
         self.output_hypernetworks_mlps = nn.ModuleList(
@@ -118,35 +122,48 @@ class MaskDecoder(nn.Module):
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Predicts masks. See 'forward' for more details."""
         # Concatenate output tokens
-        output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight], dim=0)
-        output_tokens = output_tokens.unsqueeze(0).expand(sparse_prompt_embeddings.size(0), -1, -1)
+        output_tokens = torch.cat(
+            [self.iou_token.weight, self.mask_tokens.weight], dim=0
+        )
+        output_tokens = output_tokens.unsqueeze(0).expand(
+            sparse_prompt_embeddings.size(0), -1, -1
+        )
         tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
+        masks_batches = []
+        iou_pred_batches = []
+        for i in range(image_embeddings.shape[0]):
+            # Expand per-image data in batch direction to be per-mask
+            # src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
+            src = torch.repeat_interleave(
+                image_embeddings[i][None], tokens.shape[0], dim=0
+            )
+            src = src + dense_prompt_embeddings
+            pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
+            # pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
+            b, c, h, w = src.shape
 
-        # Expand per-image data in batch direction to be per-mask
-        src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
-        src = src + dense_prompt_embeddings
-        pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
-        b, c, h, w = src.shape
+            # Run the transformer
+            hs, src = self.transformer(src, pos_src, tokens)
+            iou_token_out = hs[:, 0, :]
+            mask_tokens_out = hs[:, 1 : (1 + self.num_mask_tokens), :]
 
-        # Run the transformer
-        hs, src = self.transformer(src, pos_src, tokens)
-        iou_token_out = hs[:, 0, :]
-        mask_tokens_out = hs[:, 1 : (1 + self.num_mask_tokens), :]
+            # Upscale mask embeddings and predict masks using the mask tokens
+            src = src.transpose(1, 2).view(b, c, h, w)
+            upscaled_embedding = self.output_upscaling(src)
+            hyper_in_list: List[torch.Tensor] = []
+            for i in range(self.num_mask_tokens):
+                hyper_in_list.append(
+                    self.output_hypernetworks_mlps[i](mask_tokens_out[:, i, :])
+                )
+            hyper_in = torch.stack(hyper_in_list, dim=1)
+            b, c, h, w = upscaled_embedding.shape
+            masks = (hyper_in @ upscaled_embedding.view(b, c, h * w)).view(b, -1, h, w)
 
-        # Upscale mask embeddings and predict masks using the mask tokens
-        src = src.transpose(1, 2).view(b, c, h, w)
-        upscaled_embedding = self.output_upscaling(src)
-        hyper_in_list: List[torch.Tensor] = []
-        for i in range(self.num_mask_tokens):
-            hyper_in_list.append(self.output_hypernetworks_mlps[i](mask_tokens_out[:, i, :]))
-        hyper_in = torch.stack(hyper_in_list, dim=1)
-        b, c, h, w = upscaled_embedding.shape
-        masks = (hyper_in @ upscaled_embedding.view(b, c, h * w)).view(b, -1, h, w)
-
-        # Generate mask quality predictions
-        iou_pred = self.iou_prediction_head(iou_token_out)
-
-        return masks, iou_pred
+            # Generate mask quality predictions
+            iou_pred = self.iou_prediction_head(iou_token_out)
+            masks_batches.append(masks)
+            iou_pred_batches.append(iou_pred)
+        return torch.stack(masks_batches), torch.stack(iou_pred_batches)
 
 
 # Lightly adapted from
