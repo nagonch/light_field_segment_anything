7f65dccc767eecb536278ed4b3fd8c7f0d272ad5
nagonch <kolchindru@gmail.com>
2024-04-06 11:24:28 +1100

    epipolar lines

--- LF_functions.py
+++ LF_functions.py
@@ -6,40 +6,43 @@ from torchvision.transforms.functional import affine
 from utils import shift_binary_mask
 
 
-def calculate_shifted_metric(
-    segments,
-    main_segment_i,
-    sub_segment_i,
-    uv_shift,
-    metric=BinaryJaccardIndex().cuda(),
-):
-    mask_i = (segments == main_segment_i).to(torch.int32).sum(axis=(0, 1))
-    mask_j = (segments == sub_segment_i).to(torch.int32).sum(axis=(0, 1))
-    plt.imshow(mask_i.detach().cpu().numpy(), cmap="gray")
-    plt.savefig("mask_from.png")
-    plt.close()
-    plt.imshow(mask_j.detach().cpu().numpy(), cmap="gray")
-    plt.savefig("mask_to.png")
-    plt.close()
-    new_mask_j = shift_binary_mask(mask_j, uv_shift)
-    result_metric = metric(mask_i, new_mask_j).item()
-    return result_metric
+def calculate_peak_metric(segments, i_central, i_subview, step=0.5, eps=1e-9):
+    mask_central = (segments == i_central).to(torch.int32).sum(axis=(0, 1))
+    mask_subview = (segments == i_subview).to(torch.int32).sum(axis=(0, 1))
+    s_subview, t_subview = torch.where(
+        (segments == i_subview).to(torch.int32).sum(axis=(2, 3)) > 0
+    )
+    s_subview, t_subview = s_subview[0].item(), t_subview[0].item()
+    s_central, t_central = segments.shape[0] // 2, segments.shape[1] // 2
+    epipolar_line = (
+        torch.tensor([s_central - s_subview, t_central - t_subview]).float().cuda()
+    )
+    epipolar_line = F.normalize(epipolar_line[None])[0]
+
+
+# def calculate_shifted_metric(
+#     segments,
+#     main_segment_i,
+#     sub_segment_i,
+#     uv_shift,
+#     metric=BinaryJaccardIndex().cuda(),
+# ):
+#     mask_i = (segments == main_segment_i).to(torch.int32).sum(axis=(0, 1))
+#     mask_j = (segments == sub_segment_i).to(torch.int32).sum(axis=(0, 1))
+#     plt.imshow(mask_i.detach().cpu().numpy(), cmap="gray")
+#     plt.savefig("mask_from.png")
+#     plt.close()
+#     plt.imshow(mask_j.detach().cpu().numpy(), cmap="gray")
+#     plt.savefig("mask_to.png")
+#     plt.close()
+#     new_mask_j = shift_binary_mask(mask_j, uv_shift)
+#     result_metric = metric(mask_i, new_mask_j).item()
+#     return result_metric
 
 
 if __name__ == "__main__":
-    segments = torch.tensor(torch.load("merged.pt")).cuda()
-    segments[segments != 178183] = 0
-    num = 1
-    for i in range(segments.shape[0]):
-        for j in range(segments.shape[1]):
-            segments[i, j][segments[i, j] != 0] += num
-            num += 1
-    # print(torch.unique(segments))
-    # raise
-    shifted_metric = calculate_shifted_metric(
-        segments,
-        178184,
-        178191,
-        (0, 0),
-    )
-    print(shifted_metric)
+    segments = torch.tensor(torch.load("segments.pt")).cuda()
+    print(torch.unique(segments[0, 1]))
+    segment_central = 31804
+    segment_subview = 3235
+    calculate_peak_metric(segments, segment_central, segment_subview)
