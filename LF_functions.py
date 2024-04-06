dc12e1d44573589d2fbdfa23be87dde5ef589f06
nagonch <kolchindru@gmail.com>
2024-04-06 21:56:49 +1100

    fix

--- LF_functions.py
+++ LF_functions.py
@@ -2,14 +2,27 @@ import torch
 from torchmetrics.classification import BinaryJaccardIndex
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
-from torchvision.transforms.functional import affine
 from utils import shift_binary_mask
 
 
-def calculate_peak_metric(segments, i_central, i_subview, n_points=50, eps=1e-9):
+def calculate_peak_metric(
+    segments,
+    i_central,
+    i_subview,
+    n_points=100,
+    metric=BinaryJaccardIndex().cuda(),
+):
     u, v = segments.shape[-2:]
     mask_central = (segments == i_central).to(torch.int32).sum(axis=(0, 1))
     mask_subview = (segments == i_subview).to(torch.int32).sum(axis=(0, 1))
+    # plt.imshow(mask_central.detach().cpu().numpy())
+    # plt.show()
+    # plt.savefig("central.png")
+    # plt.close()
+    # plt.imshow(mask_subview.detach().cpu().numpy())
+    # plt.show()
+    # plt.savefig("subview.png")
+    # plt.close()
     s_subview, t_subview = torch.where(
         (segments == i_subview).to(torch.int32).sum(axis=(2, 3)) > 0
     )
@@ -18,48 +31,49 @@ def calculate_peak_metric(segments, i_central, i_subview, n_points=50, eps=1e-9)
     epipolar_line_vector = (
         torch.tensor([s_central - s_subview, t_central - t_subview]).float().cuda()
     )
-    aspect_ratio_matrix = torch.diag(torch.tensor([u, v])).float().cuda()
+    aspect_ratio_matrix = torch.diag(torch.tensor([v, u])).float().cuda()
     epipolar_line_vector = aspect_ratio_matrix @ epipolar_line_vector
     epipolar_line_vector = F.normalize(epipolar_line_vector[None])[0]
-    epipolar_line_point = (
-        torch.stack(torch.where(mask_subview == 1)).float().mean(axis=-1)
-    )
-    u_space = torch.linspace(
-        0, epipolar_line_point[0] / epipolar_line_vector[0], n_points
-    ).cuda()
-    v_space = (
-        (u_space - epipolar_line_point[0])
-        * epipolar_line_vector[1]
-        / epipolar_line_vector[0]
-    ) + epipolar_line_point[1]
-    plt.plot(u_space.detach().cpu().numpy(), v_space.detach().cpu().numpy())
-    plt.show()
-    plt.savefig("yo.png")
+    magnitudes = torch.linspace(-500, 500, n_points).cuda()
+    ious = []
+    for i, magnitude in enumerate(magnitudes):
+        vec = torch.round(epipolar_line_vector * magnitude).long()
+        mask_new = shift_binary_mask(mask_subview, vec)
+        # plt.imshow(mask_new.detach().cpu().numpy())
+        # plt.show()
+        # plt.savefig(f"masks/{str(i).zfill(3)}.png")
+        # plt.close()
+        iou = metric(mask_central, mask_new)
+        ious.append(iou.item())
+    return iou
+    # plt.plot(ious)
+    # plt.show()
+    # plt.savefig("ious.png")
+    # print(torch.tensor(ious).max())
+    # epipolar_line_point = (
+    #     torch.stack(torch.where(mask_subview == 1)).float().mean(axis=-1)
+    # )
+    # u_space = torch.linspace(
+    #     0, epipolar_line_point[0] / epipolar_line_vector[0], n_points
+    # ).cuda()
+    # v_space = (
+    #     (u_space - epipolar_line_point[0])
+    #     * epipolar_line_vector[1]
+    #     / epipolar_line_vector[0]
+    # ) + epipolar_line_point[1]
+    # print(epipolar_line_point)
+    # plt.scatter(u_space.detach().cpu().numpy(), v_space.detach().cpu().numpy())
+    # plt.show()
+    # plt.savefig("yo.png")
     # linspace =
 
 
-# def calculate_shifted_metric(
-#     segments,
-#     main_segment_i,
-#     sub_segment_i,
-#     uv_shift,
-#     metric=BinaryJaccardIndex().cuda(),
-# ):
-#     mask_i = (segments == main_segment_i).to(torch.int32).sum(axis=(0, 1))
-#     mask_j = (segments == sub_segment_i).to(torch.int32).sum(axis=(0, 1))
-#     plt.imshow(mask_i.detach().cpu().numpy(), cmap="gray")
-#     plt.savefig("mask_from.png")
-#     plt.close()
-#     plt.imshow(mask_j.detach().cpu().numpy(), cmap="gray")
-#     plt.savefig("mask_to.png")
-#     plt.close()
-#     new_mask_j = shift_binary_mask(mask_j, uv_shift)
-#     result_metric = metric(mask_i, new_mask_j).item()
-#     return result_metric
-
-
 if __name__ == "__main__":
+    from random import randint
+
     segments = torch.tensor(torch.load("segments.pt")).cuda()
-    segment_central = 31804
-    segment_subview = 597
+    unique_segments = torch.unique(segments[2, 2])
+    segment_central = unique_segments[randint(0, unique_segments.shape[0]) - 1]
+    print(segment_central)
+    segment_subview = 15397
     calculate_peak_metric(segments, segment_central, segment_subview)
