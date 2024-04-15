2703226498dd2c18fe5c6d456c33fc7247c0c5e3
nagonch <kolchindru@gmail.com>
2024-04-15 18:06:22 +1000

    fix

--- LF_ransac.py
+++ LF_ransac.py
@@ -1,4 +1,16 @@
 import torch
 
+
+def LF_ransac(
+    segments,
+    segment_num,
+    n_estimate_points=2,
+    n_iterations=20,
+    error_threshold=0.05,
+    min_inliers=50,
+):
+    pass
+
+
 if __name__ == "__main__":
     pass
