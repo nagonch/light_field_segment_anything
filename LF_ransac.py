50f35c053fa1f0d62a223e8c41e4f0feddfe89f8
nagonch <kolchindru@gmail.com>
2024-05-15 14:25:31 +1000

    precompute centroids

--- LF_ransac.py
+++ LF_ransac.py
@@ -14,6 +14,16 @@ class LF_RANSAC_segment_merger:
         self.subview_indices = get_subview_indices(self.s_size, self.t_size)
         self.central_segments = self.get_central_segments()
         self.epipolar_line_vectors = self.get_epipolar_line_vectors()
+        self.segments_centroids = self.get_segments_centroids()
+
+    @torch.no_grad()
+    def get_segments_centroids(self):
+        centroids_dict = {}
+        for segment_i in torch.unique(self.segments):
+            centroids_dict[segment_i.item()] = binary_mask_centroid(
+                self.segments == segment_i
+            )[-2:]
+        return centroids_dict
 
     @torch.no_grad()
     def get_epipolar_line_vectors(self):
@@ -120,7 +130,7 @@ class LF_RANSAC_segment_merger:
         result_segment = subview_segments[result_segment_index]
         # Get the depth of the segment
         result_similarity = similarities[result_segment_index]
-        result_centroid = binary_mask_centroid(self.segments[s, t] == result_segment)
+        result_centroid = self.segments_centroids[result_segment.item()]
         result_depth = torch.norm(result_centroid - central_mask_centroid)
         return result_segment, result_depth, result_similarity
 
@@ -133,10 +143,7 @@ class LF_RANSAC_segment_merger:
         if subview_segments is None:
             return -1
         centroids = torch.stack(
-            [
-                binary_mask_centroid(self.segments[s, t] == segment)
-                for segment in subview_segments
-            ]
+            [self.segments_centroids[segment.item()] for segment in subview_segments]
         ).cuda()  # precompute for all the segments
         target_point = central_mask_centroid + self.epipolar_line_vectors[s, t] * depth
         target_point = target_point.repeat(centroids.shape[0], 1)
@@ -148,10 +155,7 @@ class LF_RANSAC_segment_merger:
     @torch.no_grad()
     def find_matches(self, central_mask_num):
         matches = []
-        central_mask = (self.segments == central_mask_num)[
-            self.s_central, self.t_central
-        ]
-        central_mask_centroid = binary_mask_centroid(central_mask)
+        central_mask_centroid = self.segments_centroids[central_mask_num.item()]
         # 1. Sample a random s, t
         indices_shuffled = self.shuffle_indices()
         s_main, t_main = indices_shuffled[0]
@@ -159,7 +163,6 @@ class LF_RANSAC_segment_merger:
         matched_segment, depth, certainty = self.fit(
             central_mask_num, central_mask_centroid, s_main, t_main
         )
-        print(depth)
         for s, t in indices_shuffled:
             match = self.predict(central_mask_centroid, s, t, depth)
             matches.append(match)
