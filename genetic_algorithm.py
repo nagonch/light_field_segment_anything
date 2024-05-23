b8458d503c7dc10005dffdf46844908d3629a524
nagonch <kolchindru@gmail.com>
2024-05-23 14:10:29 +1000

    add fitness function

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,25 +2,27 @@ import torch
 
 
 class GeneticAlg:
-    def __init__(self, similarity_matrix, init_size=10):
-        self.init_size = init_size
+    def __init__(self, similarity_matrix):
         self.similarity_matrix = similarity_matrix
         self.subviews, self.segments = similarity_matrix.shape
 
     def initial_population(self):
         population = torch.randint(
             segments,
-            size=(
-                self.init_size,
-                subviews,
-            ),
+            size=(subviews,),
         ).cuda()
         return population
 
+    def fitness_function(self, population):
+        index_vector = population.unsqueeze(1)
+        result = torch.gather(self.similarity_matrix, 1, index_vector).squeeze(1)
+        return result
+
 
 if __name__ == "__main__":
-    subviews = 81
-    segments = 20
-    distance_matrix = torch.rand(subviews, segments).cuda()
-    alg = GeneticAlg(distance_matrix)
-    print(alg.initial_population())
+    subviews = 4
+    segments = 3
+    similarity_matrix = torch.rand(subviews, segments).cuda()
+    alg = GeneticAlg(similarity_matrix)
+    population = alg.initial_population()
+    print(alg.fitness_function(population))
