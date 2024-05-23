4a4d8be145419cc235c186c9abdc15bb07965778
nagonch <kolchindru@gmail.com>
2024-05-24 09:46:21 +1000

    fix

--- optimizer.py
+++ optimizer.py
@@ -1,9 +1,22 @@
 import numpy as np
 from scipy.optimize import differential_evolution
+from utils import masks_iou
+import torch
 
 
 class OptimizerEvolution:
-    def __init__(self, similarities):
+    def __init__(
+        self,
+        similarities,
+        segment_matrix,
+        central_segment,
+        lambda_reg=1e-3,
+        maxiter=1000,
+    ):
+        self.maxiter = maxiter
+        self.segment_matrix = segment_matrix
+        self.central_segment = central_segment
+        self.lambda_reg = lambda_reg
         self.subviews, self.segments = similarities.shape
         self.similarities = similarities
         self.bounds = [
@@ -11,21 +24,27 @@ class OptimizerEvolution:
         ] * self.subviews
 
     def loss(self, x):
-        result = self.similarities[np.arange(x.shape[0]), x.astype(np.int32)]
-        return -result.sum() - (x % 2).sum()
+        result = self.similarities[
+            torch.arange(x.shape[0]).cuda(), torch.tensor(x).cuda().long()
+        ]
+        result_value = (-result.sum()).item()
+        return result_value
 
     def run(self):
-        result = differential_evolution(self.loss, self.bounds, integrality=True)
-        return result.x
+        result = differential_evolution(
+            self.loss, self.bounds, integrality=True, maxiter=self.maxiter
+        )
+        return torch.tensor(result.x).cuda().long()
 
 
 if __name__ == "__main__":
-    subviews = 5
-    segments = 4
-    sims = np.random.uniform(size=(subviews, segments))
-    opt = OptimizerEvolution(sims)
+    sim_matrix = torch.load("sim_matrix.pt")
+    segment_matrix = torch.load("segment_matrix.pt")
+    segment_indices = torch.load("segment_indices.pt")
+    central_mask = torch.load("central_mask.pt")
+    opt = OptimizerEvolution(sim_matrix, segment_matrix, central_mask)
     result = opt.run()
-    print(sims)
     print(result)
-    print(opt.loss(result))
-    print(opt.loss(sims.argmax(axis=1)))
+    print(sim_matrix[torch.arange(sim_matrix.shape[0]), result])
+    print(sim_matrix.max(axis=1)[0])
+    print(sim_matrix[torch.arange(sim_matrix.shape[0]), result].mean())
