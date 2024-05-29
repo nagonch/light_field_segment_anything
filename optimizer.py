4f6c531139f8d6003fe4ef5404d12d9402e4216d
nagonch <kolchindru@gmail.com>
2024-05-29 13:26:55 +1000

    adding loss

--- optimizer.py
+++ optimizer.py
@@ -9,7 +9,7 @@ class GreedyOptimizer:
         segment_matrix,
         central_segment,
         segment_indices,
-        lambda_reg=1e-3,
+        lambda_reg=1e-1,
     ):
         self.segment_indices = segment_indices
         self.segment_matrix = segment_matrix
@@ -18,14 +18,22 @@ class GreedyOptimizer:
         self.n_subviews, self.n_segments = similarities.shape
         self.similarities = similarities
 
-    def loss(self, x):
-        result = self.similarities[
-            torch.arange(x.shape[0]).cuda(), torch.tensor(x).cuda().long()
-        ]
-        result_value = (-result.sum()).item()
-        return result_value
+    def loss(self, subview_ind, segment_ind, chosen_segment_inds=None):
+        result = self.similarities[subview_ind, segment_ind]
+        if chosen_segment_inds:
+            masks = [
+                self.segment_matrix[subview, segment]
+                for subview, segment in chosen_segment_inds
+            ]
+            masks += [
+                self.segment_matrix[subview_ind, segment_ind],
+            ]
+            masks = torch.stack(masks)
+            result += self.lambda_reg * masks_regularization_score(masks)
+        return result
 
     def run(self):
+        print(self.loss(0, 0, [[1, 1], [2, 2]]))
         return None
 
 
@@ -37,6 +45,3 @@ if __name__ == "__main__":
     opt = GreedyOptimizer(sim_matrix, segment_matrix, central_mask, segment_indices)
     result = opt.run()
     print(result)
-    print(sim_matrix[torch.arange(sim_matrix.shape[0]), result])
-    print(sim_matrix.max(axis=1)[0])
-    print(sim_matrix[torch.arange(sim_matrix.shape[0]), result].mean())
