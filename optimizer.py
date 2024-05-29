8af78238c34b7db2beab8b330dc3b7864a6b3568
nagonch <kolchindru@gmail.com>
2024-05-29 13:51:33 +1000

    get optimizer running

--- optimizer.py
+++ optimizer.py
@@ -27,18 +27,36 @@ class GreedyOptimizer:
             ]
             masks += [
                 self.segment_matrix[subview_ind, segment_ind],
+                self.central_segment,
             ]
             masks = torch.stack(masks)
             result += self.lambda_reg * masks_regularization_score(masks)
         return result
 
     def run(self):
-        print(self.loss(0, 0, [[1, 1], [2, 2]]))
-        return None
+        matches = []
+        chosen_segment_inds = []
+        for i in range(self.n_subviews):
+            print(i)
+            ind_num, segment_num = torch.where(sim_matrix == sim_matrix.max())
+            sim_matrix[ind_num] = torch.ones_like(sim_matrix[ind_num]) * (-torch.inf)
+            matches.append(self.segment_indices[ind_num[0], segment_num[0]])
+            chosen_segment_inds.append([ind_num[0], segment_num[0]])
+            if i < self.n_subviews - 1:
+                for candidate_i in range(self.n_subviews):
+                    for candidate_j in range(self.n_segments):
+                        if self.segment_indices[candidate_i, candidate_j] in matches:
+                            continue
+                        self.similarities[candidate_i, candidate_j] = self.loss(
+                            candidate_i,
+                            candidate_j,
+                            chosen_segment_inds=chosen_segment_inds,
+                        )
+        return matches
 
 
 if __name__ == "__main__":
-    sim_matrix = torch.load("sim_matrix.pt")
+    sim_matrix = torch.load("sim_matrix.pt")[:, :5]
     segment_matrix = torch.load("segment_matrix.pt")
     segment_indices = torch.load("segment_indices.pt")
     central_mask = torch.load("central_mask.pt")
