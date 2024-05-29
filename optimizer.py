6e1f59e64cf6077b6a8e56e8cee3abf973a77530
nagonch <kolchindru@gmail.com>
2024-05-29 13:10:57 +1000

    fix optimizer

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,4 @@
-import numpy as np
-from scipy.optimize import differential_evolution
-from utils import masks_iou
+from utils import masks_regularization_score
 import torch
 
 
@@ -17,11 +15,8 @@ class GreedyOptimizer:
         self.segment_matrix = segment_matrix
         self.central_segment = central_segment
         self.lambda_reg = lambda_reg
-        self.subviews, self.segments = similarities.shape
+        self.n_subviews, self.n_segments = similarities.shape
         self.similarities = similarities
-        self.bounds = [
-            [0, self.segments - 1],
-        ] * self.subviews
 
     def loss(self, x):
         result = self.similarities[
@@ -31,10 +26,7 @@ class GreedyOptimizer:
         return result_value
 
     def run(self):
-        result = differential_evolution(
-            self.loss, self.bounds, integrality=True, maxiter=self.maxiter
-        )
-        return torch.tensor(result.x).cuda().long()
+        return None
 
 
 if __name__ == "__main__":
@@ -42,7 +34,7 @@ if __name__ == "__main__":
     segment_matrix = torch.load("segment_matrix.pt")
     segment_indices = torch.load("segment_indices.pt")
     central_mask = torch.load("central_mask.pt")
-    opt = OptimizerEvolution(sim_matrix, segment_matrix, central_mask)
+    opt = GreedyOptimizer(sim_matrix, segment_matrix, central_mask, segment_indices)
     result = opt.run()
     print(result)
     print(sim_matrix[torch.arange(sim_matrix.shape[0]), result])
