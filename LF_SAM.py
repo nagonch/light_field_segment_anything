36797a0d33be4c32cdde3c5996f6ccb295b3b1ea
nagonch <kolchindru@gmail.com>
2024-05-31 16:47:16 +1000

    add lf sam

--- LF_SAM.py
+++ LF_SAM.py
@@ -1,11 +1,11 @@
 import torch
-from torch import nn
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 from utils import SAM_CONFIG
 from data import HCIOldDataset
 from torchvision.transforms.functional import resize
-import numpy as np
 from torch.nn import functional as F
+import os
+from tqdm import tqdm
 
 
 class SimpleSAM:
@@ -57,6 +57,52 @@ class SimpleSAM:
         embeddings = self.get_masks_embeddings(masks, img_embedding)
         return masks, embeddings
 
+    @torch.no_grad()
+    def segment_LF(self, LF):
+        os.makedirs("embeddings", exist_ok=True)
+        result_masks = []
+        max_segment_num = -1
+        s, t, u, v, c = LF.shape
+        LF = LF.reshape(-1, LF.shape[2], LF.shape[3], LF.shape[4])
+        for subview_num, subview in tqdm(enumerate(LF)):
+            masks, embeddings = self.forward(subview)
+            item = zip(masks, embeddings)
+            masks = sorted(item, key=(lambda x: x[0].sum()), reverse=True)
+            segments = torch.stack([torch.tensor(mask[0]).cuda() for mask in masks])
+            embeddings = [torch.tensor(mask[1]).cuda() for mask in masks]
+            segments_result = (
+                torch.zeros((segments.shape[-2], segments.shape[-1])).cuda().long()
+            )
+            embedding_keys = []
+            embedding_values = []
+            segment_num = 0
+            for embedding, segment in zip(embeddings, segments):
+                segments_result[segment] = segment_num + 1
+                embedding_keys.append(segment_num + 1)
+                segment_num += 1
+                embedding_values.append(
+                    (
+                        embedding,
+                        subview_num,
+                    )
+                )
+            segments = segments_result
+            segments[segments != 0] += max_segment_num
+            embedding_keys = [int(key + max_segment_num) for key in embedding_keys]
+            embeddings_map = dict(zip(embedding_keys, embedding_values))
+            torch.save(
+                embeddings_map,
+                f"embeddings/{str(subview_num).zfill(4)}.pt",
+            )
+            del embeddings_map
+            max_segment_num = segments.max()
+            result_masks.append(segments)
+
+        result_masks = torch.stack(result_masks).reshape(
+            s, t, segments_result.shape[-2], segments_result.shape[-1]
+        )
+        return result_masks
+
 
 def get_sam():
     sam = sam_model_registry["vit_h"](checkpoint=SAM_CONFIG["model-path"])
@@ -70,4 +116,4 @@ if __name__ == "__main__":
     simple_sam = get_sam()
     HCI_dataset = HCIOldDataset()
     LF, depth, labels = HCI_dataset.get_scene("horses")
-    simple_sam.forward(LF[0, 0])
+    simple_sam.segment_LF(LF)
