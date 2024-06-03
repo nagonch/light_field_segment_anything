68ede803beb741f9ed7556c3ce33b93e884a639b
nagonch <kolchindru@gmail.com>
2024-06-03 16:23:47 +1000

    metrics

--- metrics.py
+++ metrics.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
+from data import HCIOldDataset
+import torch
+
+
+def labels_per_pixel():
     pass
+
+
+if __name__ == "__main__":
+    dataset = HCIOldDataset()
+    LF = dataset.get_scene("papillon")
+    disparity = dataset.get_disparity("papillon")
+    masks = torch.load("merged.pt")
+    print(LF.shape, disparity.shape, masks.shape)
