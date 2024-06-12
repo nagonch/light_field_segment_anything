776ebd935214fc504f0dad5eead3c753f902de8e
nagonch <kolchindru@gmail.com>
2024-06-12 13:51:48 +1000

    add metric

--- metrics.py
+++ metrics.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from scipy.io import savemat
 from collections import defaultdict
 from tqdm import tqdm
+from utils import visualize_segmentation_mask
 
 
 def get_set():
@@ -12,34 +13,34 @@ def get_set():
 
 def labels_per_pixel(labels, disparity):
     s_size, t_size, u_size, v_size = labels.shape
-    m = defaultdict(get_set)
-    for s in tqdm(range(s_size)):
-        for t in tqdm(range(t_size)):
-            for u in tqdm(range(u_size)):
-                for v in range(v_size):
-                    label = labels[s, t, u, v]
-                    u_center = (u + disparity[s, t, u, v, 0]).long()
-                    v_center = (v + disparity[s, t, u, v, 1]).long()
-                    if (
-                        u_center >= 0
-                        and v_center >= 0
-                        and u_center < u_size
-                        and v_center < v_size
-                    ):
-                        m[f"{str(u_center).zfill(4)}_{str(v_center).zfill(4)}"].add(
-                            label.item()
-                        )
-    pixel_lengths = []
-    for list in m.values():
-        pixel_lengths.append(len(list))
-    metric = torch.tensor(pixel_lengths).cuda().float().mean()
-    return metric.item()
+    u_space, v_space = torch.meshgrid(
+        (torch.arange(u_size).cuda(), torch.arange(v_size).cuda())
+    )
+    labels_projected = torch.zeros_like(labels).cuda()
+    for s in range(s_size):
+        for t in range(t_size):
+            disp = disparity[s, t]
+            l = labels[s, t]
+            u_st = (u_space + disp[:, :, 0]).reshape(-1).long()
+            v_st = (v_space + disp[:, :, 1]).reshape(-1).long()
+            mask = (u_st >= 0) & (u_st < u_size) & (v_st >= 0) & (v_st < v_size)
+            labels_projected[s, t, u_st[mask], v_st[mask]] = l[
+                u_space.reshape(-1)[mask], v_space.reshape(-1)[mask]
+            ]
+    labels_projected = labels_projected.reshape(s_size * t_size, u_size * v_size).T
+    lengths = []
+    for label_set in labels_projected:
+        lengths.append(len(set(label_set.tolist())))
+    result = torch.tensor(lengths).cuda().float().mean()
+    return result
 
 
 if __name__ == "__main__":
     dataset = HCIOldDataset()
     LF = dataset.get_scene("papillon")
     disparity = torch.tensor(dataset.get_disparity("papillon")).cuda()
-    labels = torch.tensor(torch.load("merged.pt")).cuda()
+    labels = torch.tensor(torch.load("past_merges/merged_papillon.pt")).cuda()
     result = labels_per_pixel(labels, disparity)
+    visualize_segmentation_mask(labels.detach().cpu().numpy(), None)
+    visualize_segmentation_mask(result.detach().cpu().numpy(), None)
     print(result)
