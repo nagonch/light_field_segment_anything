852077a12525f1f3708abc358d3992004eb5b360
nagonch <kolchindru@gmail.com>
2024-06-20 13:54:00 +1000

    not remapping labels

--- data.py
+++ data.py
@@ -85,7 +85,8 @@ class UrbanLFDataset(Dataset):
                 )
                 .cuda()
             )
-            labels = remap_labels(labels) + 1
+            # labels = remap_labels(labels)
+            labels += 1
             return_tuple.append(labels)
         return return_tuple
 
