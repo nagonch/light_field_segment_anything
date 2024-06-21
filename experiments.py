88e2859d076f36733f065cba784612f4949a6864
nagonch <kolchindru@gmail.com>
2024-06-21 14:11:46 +1000

    experiment py

---
+++ experiments.py
@@ -0,0 +1,5 @@
+# 1. Load data (LF + gt labels. Datasets: HCI, UrbanLF_Syn_val, UrbanLF_Real_val)
+# 2. Create folder with f"{experiment_name}". Save both configs there
+# 3. Segment dataset with SAM and save corresponding lightfields and embeddings
+# 4. Merge segments and save the results
+# 5. Calculate metrics and save them to table
