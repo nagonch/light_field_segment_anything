4c635bf066d7f98cf2f6ccc2e27dded4b33e8375
nagonch <kolchindru@gmail.com>
2024-07-26 12:06:28 +1000

    xmem

--- LF_Xmem.py
+++ LF_Xmem.py
@@ -1,4 +1,5 @@
 from data import HCIOldDataset
+from LF_SAM import get_sam
 
 
 def lawnmower(LF):
@@ -17,17 +18,21 @@ def lawnmower(LF):
     num_inds = len(index_sequence)
     ind_sequence_up = index_sequence[: num_inds // 2][::-1]
     ind_sequence_down = index_sequence[num_inds // 2 + 1 :]
-    print(ind_sequence_up, ind_sequence_down)
-    return index_sequence
+    return ind_sequence_up, ind_sequence_down
 
 
 def xmem_LF(LF):
-    return LF
+    s, t, u, v, c = LF.shape
+    central_subap = LF[s // 2, t // 2]
+    sam = get_sam()
+    masks = sam.generator.generate(central_subap)
+    return masks
 
 
 if __name__ == "__main__":
     dataset = HCIOldDataset()
     LF, _, _ = dataset[0]
-    ind_pattern = lawnmower(LF)
-    print(ind_pattern[len(ind_pattern) // 2])
-    # print(type(LF))
+    xmem_LF(LF)
+    # ind_pattern = lawnmower(LF)
+    # print(ind_pattern[len(ind_pattern) // 2])
+    # # print(type(LF))
