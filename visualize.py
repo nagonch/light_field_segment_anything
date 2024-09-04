f7e07df0c94adc292649fb658b893b546ede0a64
Nikolai Goncharov <kolchindru@gmail.com>
2024-09-04 14:41:47 +1000

    fix

--- visualize.py
+++ visualize.py
@@ -4,6 +4,7 @@ from experiments import get_datset
 from plenpy.lightfields import LightField
 import torch
 import argparse
+from utils import vis_to_gif
 
 
 def visualize(frame_number):
@@ -11,7 +12,7 @@ def visualize(frame_number):
     LF_data, _, _ = dataset[frame_number]
     print("visualizing light field image")
     LF = LightField(LF_data)
-    LF.show()
+    # LF.show()
     frame_number = str(frame_number).zfill(4)
     exp_name = EXP_CONFIG["exp-name"]
 
@@ -19,12 +20,18 @@ def visualize(frame_number):
     if os.path.exists(merged_segments_filename):
         merged_segments = torch.load(merged_segments_filename).cpu().numpy()
         print("visualizing merged segments")
-        visualize_segmentation_mask(merged_segments, LF_data)
-        visualize_segmentation_mask(merged_segments, LF_data, only_boundaries=True)
+        vis = visualize_segmentation_mask(merged_segments, LF_data, just_return=True)
+        # visualize_segmentation_mask(merged_segments, LF_data, only_boundaries=True)
+        return vis
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("frame_number", type=int)
-    args = parser.parse_args()
-    visualize(args.frame_number)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("frame_number", type=int)
+    # args = parser.parse_args()
+    gifs_dir = f'{EXP_CONFIG["exp-name"]}_gifs'
+    os.makedirs(gifs_dir, exist_ok=True)
+    for i in range(28):
+        vis = visualize(i)
+        vis_to_gif(visualize(i), f"{gifs_dir}/{str(i).zfill(4)}.gif")
+        raise
