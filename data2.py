0284f48e724f0726c5df84960ac3cd3ffdf1f521
Nikolai Goncharov <kolchindru@gmail.com>
2024-10-25 10:58:32 +1100

    add data2

---
+++ data2.py
@@ -0,0 +1,68 @@
+import torch
+import numpy as np
+from PIL import Image
+import os
+import math
+import h5py
+from plenpy.lightfields import LightField
+
+
+class UrbanLFSynDataset:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.frames = sorted(
+            [
+                item
+                for item in os.listdir(self.data_path)
+                if os.path.isdir(f"{self.data_path}/{item}")
+            ]
+        )
+        self.size = len(self.frames)
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        frame = self.frames[idx]
+        imgs = []
+        disparities = []
+        labels = []
+        for filename in sorted(os.listdir(f"{self.data_path}/{frame}")):
+            if (
+                filename.endswith("depth.png")
+                or filename.endswith("disparity.png")
+                or filename.endswith("label.png")
+            ):
+                continue
+            if filename.endswith(".png"):
+                img = np.array(Image.open(f"{self.data_path}/{frame}/{filename}"))
+                img = (torch.tensor(img))[:, :, :3]
+                imgs.append(img)
+            elif filename.endswith("disparity.npy"):
+                disparities.append(
+                    torch.tensor(np.load(f"{self.data_path}/{frame}/{filename}"))
+                )
+            elif filename.endswith("label.npy"):
+                labels.append(
+                    torch.tensor(np.load(f"{self.data_path}/{frame}/{filename}"))
+                )
+        LF = np.stack(imgs)
+        n_apertures = int(math.sqrt(LF.shape[0]))
+        u, v, c = LF.shape[-3:]
+        LF = LF.reshape(
+            n_apertures,
+            n_apertures,
+            u,
+            v,
+            c,
+        )
+        LF = np.flip(LF, axis=(0, 1))
+        return LF
+
+
+if __name__ == "__main__":
+    dataset = UrbanLFSynDataset(
+        "/home/nagonch/repos/LF_object_tracking/UrbanLF_Syn/val"
+    )
+    for LF in dataset:
+        LightField(LF).show()
