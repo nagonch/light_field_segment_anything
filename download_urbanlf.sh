29131dceb04d53fd4184227bbca3b425b1eb8ed2
Nikolai Goncharov <kolchindru@gmail.com>
2024-11-02 02:00:46 +1100

    fix

--- download_urbanlf.sh
+++ download_urbanlf.sh
@@ -3,9 +3,3 @@ mkdir UrbanLF_Syn
 unzip val.zip
 mv val UrbanLF_Syn/val
 rm val.zip
-
-gdown --id 15OF7OaJH9L3ztlY6bIRBvHIurDOhgWCM
-mkdir UrbanLF_Real
-unzip val.zip
-mv val UrbanLF_Real/val
-rm val.zip
